ac9ff66477befaaeef590867906b85cddc3b8b82
arshiaTests <arshia.org79@gmail.com>
2025-02-05 13:44:21 +0330

    math.max method

--- ex2.js
+++ ex2.js
@@ -1,20 +1,4 @@
-function maxNum(arr){
-    if(arr.length === 0)return"array is empty"
-    let max = 0
-    for( let i = 0 ; i<arr.length ; i++){
-        if(arr[i]>max){
-            max = arr[i];
-        } 
-    }
-    return max ;
+function max (arr){
+    return Math.max(...arr);
 }
-console.log(maxNum([2,4,3,8,5,1,2,4]))
- function y(n){
-    let jam = 0;
-    for(let i = 0 ; i<n.length; i++){
-        if(n[i]>jam){
-            jam = n[i]
-        }
-    }
-    return jam;
- }
+console.log(max([1,4,2,5,67,4,3,7,8,4]))
